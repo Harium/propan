7d6b8c6de561f35fcf025d10817545f66ec3e9c5
Yuri Pourre <yuripourre@gmail.com>
2019-05-01 00:05:30 -0300

    Add draft triangulation

--- src/main/java/com/harium/propan/core/model/Model.java
+++ src/main/java/com/harium/propan/core/model/Model.java
@@ -5,10 +5,7 @@ import com.badlogic.gdx.math.Vector3;
 import com.harium.propan.core.material.OBJMaterial;
 import com.harium.propan.geometry.BoundingBox3D;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Model {
 
@@ -166,6 +163,105 @@ public class Model {
             v.add(x, y, z);
         }
     }
+
+    public void triangulate() {
+        ListIterator<Face> iterator = faces.listIterator();
+
+        while (iterator.hasNext()) {
+            Face face = iterator.next();
+            if (face.getSides() == 3) {
+                continue;
+            } else {
+                if (face.getSides() == 4) {
+                    Face nextFace = new Face(3);
+
+                    nextFace.vertexIndex[0] = face.vertexIndex[1];
+                    nextFace.vertexIndex[1] = face.vertexIndex[2];
+                    nextFace.vertexIndex[2] = face.vertexIndex[3];
+                    face.vertexIndex = new int[]{face.vertexIndex[0], face.vertexIndex[1], face.vertexIndex[3]};
+
+                    if (face.normalIndex != null) {
+                        nextFace.normalIndex[0] = face.normalIndex[1];
+                        nextFace.normalIndex[1] = face.normalIndex[2];
+                        nextFace.normalIndex[2] = face.normalIndex[3];
+                        face.normalIndex = new int[]{face.normalIndex[0], face.normalIndex[1], face.normalIndex[3]};
+                    }
+
+                    if (face.textureIndex != null) {
+                        nextFace.textureIndex[0] = face.textureIndex[1];
+                        nextFace.textureIndex[1] = face.textureIndex[2];
+                        nextFace.textureIndex[2] = face.textureIndex[3];
+                        face.textureIndex = new int[]{face.textureIndex[0], face.textureIndex[1], face.textureIndex[3]};
+                    }
+
+                    face.setSides(3);
+                    iterator.add(nextFace);
+                } else {
+                    System.err.println("Propan cannot triangulate face with more than 4 faces.");
+                    /*List<Face> triangles = computeTriangles(this, face);
+                    iterator.remove();
+                    for (Face f : triangles) {
+                        iterator.add(f);
+                    }*/
+                }
+            }
+        }
+    }
+
+    /*private List<Face> computeTriangles(Model model, Face face) {
+        Map<Vector3, Integer> position = new HashMap<>();
+        Map<Vector3, Integer> normal = new HashMap<>();
+        Map<Vector3, Integer> texture = new HashMap<>();
+
+        List<Vector3> points = new ArrayList<>();
+
+        boolean hasNormal = face.normalIndex == null;
+        boolean hasTexture = face.textureIndex == null;
+
+        double[] data = new double[face.getSides() * 3];
+
+        for (int i = 0; i < face.getSides(); i++) {
+            int index = face.vertexIndex[i];
+            Vector3 p = model.getVertices().get(index);
+            points.add(p);
+            position.put(p, index);
+
+            if (hasNormal) {
+                normal.put(p, face.normalIndex[i]);
+            }
+            if (hasTexture) {
+                texture.put(p, face.textureIndex[i]);
+            }
+
+            data[i * 3] = p.x;
+            data[i * 3 + 1] = p.y;
+            data[i * 3 + 2] = p.z;
+        }
+
+        List<Face> faces = new ArrayList<>();
+
+        Triangulation triangulation = new DelaunayTriangulation();
+        List<Triangle> triangles = triangulation.triangulate(points);
+
+        for (Triangle tri : triangles) {
+            Face f = new Face(3);
+            updateA(f, tri.getA(), 0, position, normal, texture, hasNormal, hasTexture, tri);
+            updateA(f, tri.getB(), 1, position, normal, texture, hasNormal, hasTexture, tri);
+            updateA(f, tri.getC(), 2, position, normal, texture, hasNormal, hasTexture, tri);
+        }
+
+        return faces;
+    }
+
+    private void updateA(Face f, Vector3 point, int index, Map<Vector3, Integer> position, Map<Vector3, Integer> normal, Map<Vector3, Integer> texture, boolean hasNormal, boolean hasTexture, Triangle tri) {
+        f.vertexIndex[index] = position.get(point);
+        if (hasNormal) {
+            f.normalIndex[index] = normal.get(point);
+        }
+        if (hasTexture) {
+            f.textureIndex[index] = texture.get(point);
+        }
+    }*/
 
     public String getPath() {
         return path;
--- src/test/java/com/harium/propan/core/model/ModelTest.java
+++ src/test/java/com/harium/propan/core/model/ModelTest.java
@@ -17,8 +17,8 @@ public class ModelTest {
 
     @Test
     public void testCentroid() {
-        model.addVertex(new Vector3(1,1,1));
-        model.addVertex(new Vector3(-1,-1,-1));
+        model.addVertex(new Vector3(1, 1, 1));
+        model.addVertex(new Vector3(-1, -1, -1));
 
         Vector3 centroid = model.centroid();
         Assert.assertEquals(0, centroid.x, EPSILON);
@@ -30,7 +30,7 @@ public class ModelTest {
     public void testRotate() {
         float angle = 90;
 
-        model.addVertex(new Vector3(1,0,0));
+        model.addVertex(new Vector3(1, 0, 0));
         model.rotate(Vector3.Y, angle);
 
         Assert.assertEquals(0, model.getVertices().get(0).x, EPSILON);
@@ -42,8 +42,8 @@ public class ModelTest {
     public void testRotateWithOrigin() {
         float angle = 90;
 
-        model.addVertex(new Vector3(2,0,0));
-        model.rotate(Vector3.Y, angle, new Vector3(1,0,0));
+        model.addVertex(new Vector3(2, 0, 0));
+        model.rotate(Vector3.Y, angle, new Vector3(1, 0, 0));
 
         Assert.assertEquals(1, model.getVertices().get(0).x, EPSILON);
         Assert.assertEquals(0, model.getVertices().get(0).y, EPSILON);
@@ -52,7 +52,7 @@ public class ModelTest {
 
     @Test
     public void testScale() {
-        model.addVertex(new Vector3(1,1,1));
+        model.addVertex(new Vector3(1, 1, 1));
         model.scale(2);
 
         Assert.assertEquals(2, model.getVertices().get(0).x, EPSILON);
@@ -63,12 +63,67 @@ public class ModelTest {
 
     @Test
     public void testTranslate() {
-        model.addVertex(new Vector3(0,0,0));
-        model.translate(1,0,0);
+        model.addVertex(new Vector3(0, 0, 0));
+        model.translate(1, 0, 0);
 
         Assert.assertEquals(1, model.getVertices().get(0).x, EPSILON);
         Assert.assertEquals(0, model.getVertices().get(0).y, EPSILON);
         Assert.assertEquals(0, model.getVertices().get(0).z, EPSILON);
     }
+
+    @Test
+    public void testTriangulateSquare() {
+        int[] squareOrder = new int[]{0, 1, 2, 3};
+        Face square = new Face(4);
+        square.vertexIndex = squareOrder;
+        square.normalIndex = squareOrder;
+        square.textureIndex = squareOrder;
+
+        model.getFaces().add(square);
+        Assert.assertEquals(1, model.getFaces().size());
+
+        model.triangulate();
+        Assert.assertEquals(2, model.getFaces().size());
+        Assert.assertEquals(3, model.getFaces().get(0).getSides());
+        Assert.assertEquals(3, model.getFaces().get(1).getSides());
+
+        Assert.assertArrayEquals(new int[]{0, 1, 3}, model.getFaces().get(0).vertexIndex);
+        Assert.assertArrayEquals(new int[]{0, 1, 3}, model.getFaces().get(0).normalIndex);
+        Assert.assertArrayEquals(new int[]{0, 1, 3}, model.getFaces().get(0).textureIndex);
+
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, model.getFaces().get(1).vertexIndex);
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, model.getFaces().get(1).normalIndex);
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, model.getFaces().get(  1).textureIndex);
+    }
+
+    /*@Test
+    public void testTriangulatePolygon() {
+        model.getVertices().add(new Vector3(0,0,0));
+        model.getVertices().add(new Vector3(1,1,0));
+        model.getVertices().add(new Vector3(2,2,0));
+        model.getVertices().add(new Vector3(3,3,0));
+        model.getVertices().add(new Vector3(4,4,0));
+
+        Face polygon = new Face(5);
+        polygon.vertexIndex = new int[]{0, 1, 2, 3, 4};
+        polygon.normalIndex = new int[]{5, 6, 7, 8, 9};
+        polygon.textureIndex = new int[]{10, 11, 12, 13, 14};
+
+        model.getFaces().add(polygon);
+        Assert.assertEquals(1, model.getFaces().size());
+
+        model.triangulate();
+        Assert.assertEquals(3, model.getFaces().size());
+        Assert.assertEquals(3, model.getFaces().get(0).getSides());
+        Assert.assertEquals(3, model.getFaces().get(1).getSides());
+
+        Assert.assertArrayEquals(new int[]{0, 1, 3}, model.getFaces().get(0).vertexIndex);
+        Assert.assertArrayEquals(new int[]{0, 1, 3}, model.getFaces().get(0).normalIndex);
+        Assert.assertArrayEquals(new int[]{0, 1, 3}, model.getFaces().get(0).textureIndex);
+
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, model.getFaces().get(1).vertexIndex);
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, model.getFaces().get(1).normalIndex);
+        Assert.assertArrayEquals(new int[]{1, 2, 3}, model.getFaces().get(1).textureIndex);
+    }*/
 
 }
